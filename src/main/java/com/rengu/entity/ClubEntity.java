e8edfc49e6755a03ed63c477970463e22483be52
韩常铭 <han.changming@qq.com>
2017-05-18 17:19:40 +0800

    修复了之前不能使用HQL语句的问题，增加了提交数据库的方法

---
+++ src/main/java/com/rengu/entity/ClubEntity.java
@@ -0,0 +1,53 @@
+package com.rengu.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by hanchangming on 2017/5/18.
+ */
+@Entity
+@Table(name = "club", schema = "TestDatabase")
+public class ClubEntity {
+    private String id;
+    private String name;
+
+    @Id
+    @Column(name = "id", nullable = false, length = 255)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 255)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClubEntity that = (ClubEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
