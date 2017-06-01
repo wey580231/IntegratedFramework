542fd10b25fb847c6bed6ad959bfdade1d7cddbc
name <1925654299@gmail.com>
2017-06-01 16:11:04 +0800

    服务写到app.js

--- web/app.js
+++ web/app.js
@@ -23,7 +23,106 @@ angular.module("IntegratedFramework", [
     'IntegratedFramework.ScheduleSnapController',
     'IntegratedFramework.InteractiveController',
     'IntegratedFramework.GuideController'
-]).config(['$routeProvider', function ($routeProvider, $locationProvider) {
-    // $locationProvider.hashPrefix('!');
-    // $routeProvider.otherwise({redirectTo: '/jsps/OrderManagement/OnlineManagement'});
-}]);
+])
+/*.config(['$routeProvider', function ($routeProvider, $locationProvider) {
+ // $locationProvider.hashPrefix('!');
+ // $routeProvider.otherwise({redirectTo: '/jsps/OrderManagement/OnlineManagement'});
+ }])*/
+    .config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        $httpProvider.defaults.transformRequest = function (obj) {
+            var str = [];
+            for (var p in obj) {
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            }
+            return str.join("&");
+        }
+    }])
+    .factory("myHttpService", ['$http', function ($http) {
+        var service = {};
+        var _get = function (servletUrl) {
+            return $http({
+                'method': 'get',
+                'dataType': 'json',
+                'contentType': 'application/json;charset=UTF-8',
+                'url': servletUrl,
+            });
+        };
+        var _post = function (servletUrl, body) {
+            return $http({
+                'method': 'post',
+                'dataType': 'json',
+                'contentType': 'application/json;charset=UTF-8',
+                'url': servletUrl,
+                'data': body
+            });
+        };
+        var _delete = function (servletUrl,body) {
+            return $http({
+                'method': 'delete',
+                'dataType': 'json',
+                'contentType': 'application/json;charset=UTF-8',
+                'url': servletUrl,
+                'data': body
+            });
+        }
+        service.get = _get;
+        service.post = _post;
+        service.delete = _delete;
+        return service;
+    }])
+    /* .factory("myHttpService", function ($http) {
+     var service = {
+     get : function () {
+     $http({
+     'method': 'get',
+     'dataType': 'json',
+     'contentType': 'application/json;charset=UTF-8',
+     'url': servletUrl,
+     'headers': {
+     'url': requestUrl
+     }
+     });
+     },
+
+     post : function () {
+     $http({
+     'method': 'post',
+     'dataType': 'json',
+     'contentType': 'application/json;charset=UTF-8',
+     'url': 'http://localhost:8080/orders/getAllOrders.action',
+     'transformRequest': function (obj) {
+     var str = [];
+     for (var p in obj) {
+     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+     }
+     return str.join("&");
+     }
+     }, "json")
+     },
+
+     delete : function () {
+     $http({
+     'method': 'delete',
+     'dataType': 'json',
+     'contentType': 'application/json;charset=UTF-8',
+     'url': servletUrl,
+     'headers': {
+     'url': requestUrl
+     }
+     }, "json").then(function successCallback(response) {
+     return response.data;
+     });
+     }};
+     return service;
+     })
+     */
+
+    .factory("serviceList", function () {
+        var service = {};
+        var backUrl = "http://localhost:8080/";
+        service.ListOrder = backUrl + "orders/getAllOrders.action";
+        return service;
+    })
+
