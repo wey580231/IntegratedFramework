4d74e435cda61a2c6aefa20926c95267f0c24d80
XYmar <xqingliuwu@163.com>
2017-07-14 14:17:22 +0800

    异常模拟

--- web/pages/AdjustAnalysis/AdjustOrderController.js
+++ web/pages/AdjustAnalysis/AdjustOrderController.js
@@ -10,7 +10,10 @@ angular.module("IntegratedFramework.AdjustOrderController", ['ngRoute'])
         })
     }])
 
-    .controller('AdjustOrderController', function ($scope, $http, myHttpService, serviceList,renderTableService) {
+    .controller('AdjustOrderController', function ($scope, $http, myHttpService, serviceList,validate,renderTableService) {
+
+        var addData = [];
+
         myHttpService.get(serviceList.AdjustOrder).then(function (response) {
             $scope.adjustOrder = response.data;
         });
@@ -18,5 +21,87 @@ angular.module("IntegratedFramework.AdjustOrderController", ['ngRoute'])
         //渲染checkBox样式
         $scope.renderTable = function ($last) {
             renderTableService.renderTable($last);
+        };
+
+        //Date picker
+        $('#modal-add-t0-datepicker').datepicker({
+            format: "yyyy/mm/dd",
+            todayHighlight: true,
+            autoclose: true
+        });
+        $('#modal-add-t1-datepicker').datepicker({
+            format: "yyyy/mm/dd",
+            todayHighlight: true,
+            autoclose: true
+        });
+        $('#modal-add-t2-datepicker').datepicker({
+            format: "yyyy/mm/dd",
+            todayHighlight: true,
+            autoclose: true
+        });
+
+        //信息填写检验
+        var orderAddValidate = function () {
+            var params = {};
+            var t0 = $("input[id='modal-add-t0-datepicker']").val();
+            var t1 = $("input[id='modal-add-t1-datepicker']").val();
+            var t2 = $("input[id='modal-add-t2-datepicker']").val();
+            params.t0 = Date.parse($("input[id='modal-add-t0-datepicker']").val());
+            params.t1 = Date.parse($("input[id='modal-add-t1-datepicker']").val());
+            params.t2 = Date.parse($("input[id='modal-add-t2-datepicker']").val());
+            addData = JSON.stringify(params);
+
+            if (!validate.checkLength(t0)) {
+                $("#add-t0").removeClass("has-success");
+                $("#add-t0").addClass("has-error");
+            } else {
+                $("#add-t0").removeClass("has-error");
+                $("#add-t0").addClass(" has-success");
+            }
+
+            if (!validate.checkLength(t0)) {
+                $("#add-t1").removeClass("has-success");
+                $("#add-t1").addClass("has-error");
+            } else {
+                $("#add-t1").removeClass("has-error");
+                $("#add-t1").addClass(" has-success");
+            }
+
+            if (!validate.checkLength(t2)) {
+                $("#add-t2").removeClass("has-success");
+                $("#add-t2").addClass("has-error");
+            } else {
+                $("#add-t2").removeClass("has-error");
+                $("#add-t2").addClass(" has-success");
+            }
+
+            if (validate.checkLength(params.t0) && validate.checkLength(params.t1) && validate.checkLength(params.t2)) {
+                return true;
+            } else {
+
+                return false;
+            }
+        };
+
+        //新增订单
+        $scope.addAdjustOrder = function () {
+            if (orderAddValidate()) {
+                $("#modal-add").modal('hide');
+                console.log(addData);
+                myHttpService.post(serviceList.AddAdjustOrder, addData).then(function successCallback() {
+                    //setTimeout('window.location.reload();', 0.1);
+                }, function errorCallback() {
+                    notification.sendNotification("alert", "请求失败");
+                })
+            } else {
+                notification.sendNotification("alert", "参数错误");
+            }
+        };
+
+
+        $scope.reset = function () {
+            $("input").val('');
+            $("div").removeClass("has-error");
+            $("div").removeClass("has-success");
         };
     });
