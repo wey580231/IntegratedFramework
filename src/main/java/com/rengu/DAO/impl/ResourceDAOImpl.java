02d4b55e8730db8c171c8b347e85e329a6e79db8
XYmar <xqingliuwu@163.com>
2017-09-25 18:50:50 +0800

    班次反向绑定

--- src/main/java/com/rengu/DAO/impl/ResourceDAOImpl.java
+++ src/main/java/com/rengu/DAO/impl/ResourceDAOImpl.java
@@ -2,12 +2,14 @@ package com.rengu.DAO.impl;
 
 import com.rengu.DAO.ResourceDAO;
 import com.rengu.entity.RG_ResourceEntity;
+import com.rengu.entity.RG_ShiftEntity;
 import com.rengu.util.MySessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by hanchangming on 2017/5/31.
@@ -48,6 +50,76 @@ public class ResourceDAOImpl extends SuperDAOImpl implements ResourceDAO<RG_Reso
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
+        }
+    }
+
+    @Override
+    public RG_ResourceEntity findAllById(Session session, String id) {
+        try {
+            String hql = "from RG_ResourceEntity rg_resourceEntity where rg_resourceEntity.id =:id";
+            Query query = session.createQuery(hql);
+            query.setParameter("id", id);
+            if (!query.list().isEmpty()) {
+                RG_ResourceEntity rg_resourceEntity = (RG_ResourceEntity) query.list().get(0);
+                return rg_resourceEntity;
+            } else {
+                return null;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean update(Session session, RG_ResourceEntity rg_resourceEntity) {
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                transaction = session.beginTransaction();
+            }
+
+
+
+            session.merge(rg_resourceEntity);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        /*Transaction transaction = session.getTransaction();
+        if (!transaction.isActive()) {
+            transaction = session.beginTransaction();
+        }
+
+        Set<RG_ShiftEntity> rg_shiftEntitySet = rg_resourceEntity.getShiftsById();
+        if (rg_shiftEntitySet != null) {
+            for (RG_ShiftEntity rg_shiftEntity : rg_shiftEntitySet) {
+                rg_shiftEntity.getResources().remove(rg_resourceEntity);
+                session.update(rg_resourceEntity);
+            }
+
+            transaction.commit();
+            return true;
+        } else {
+            return false;
+        }*/
+
+    }
+
+    public boolean save(Session session,Object object) {
+        try {
+            Transaction transaction = session.getTransaction();
+            if (!transaction.isActive()) {
+                transaction = session.beginTransaction();
+            }
+            session.saveOrUpdate(object);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
--- src/main/java/com/rengu/actions/ResourceAction.java
+++ src/main/java/com/rengu/actions/ResourceAction.java
@@ -3,12 +3,19 @@ package com.rengu.actions;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.rengu.DAO.ResourceDAO;
 import com.rengu.DAO.impl.ResourceDAOImpl;
+import com.rengu.entity.RG_DistanceEntity;
 import com.rengu.entity.RG_ResourceEntity;
+import com.rengu.entity.RG_ShiftEntity;
 import com.rengu.util.DAOFactory;
+import com.rengu.util.MySessionFactory;
 import com.rengu.util.Tools;
 import com.rengu.util.WebSocketNotification;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by hanchangming on 2017/5/31.
@@ -22,15 +29,47 @@ public class ResourceAction extends SuperAction {
         Tools.jsonPrint(jsonString, this.httpServletResponse);
     }
 
-    public void save() throws Exception {
+    /*public void save() throws Exception {
+        Session session = MySessionFactory.getSessionFactory().openSession();
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+
         String jsonString = Tools.getHttpRequestBody(httpServletRequest);
         RG_ResourceEntity rg_resourceEntity = Tools.jsonConvertToEntity(jsonString, RG_ResourceEntity.class);
         rg_resourceEntity.setIdR(Tools.getUUID());
         ResourceDAOImpl resourceDAOInstance = DAOFactory.getResourceInstance();
         if (resourceDAOInstance.save(rg_resourceEntity)) {
+
         } else {
             WebSocketNotification.broadcast(Tools.creatNotificationMessage("Resource保存失败", "alert"));
         }
+    }*/
+
+    public void save() throws Exception {
+        Session session = MySessionFactory.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+
+        String jsonString = Tools.getHttpRequestBody(httpServletRequest);
+        RG_ResourceEntity rg_resourceEntity = Tools.jsonConvertToEntity(jsonString, RG_ResourceEntity.class);
+        rg_resourceEntity.setIdR(Tools.getUUID());
+
+        String nameShift = rg_resourceEntity.getNameShift();
+        RG_ShiftEntity rg_shiftEntity = (RG_ShiftEntity)session.createQuery("select shift from RG_ShiftEntity shift where shift.name =:name").setParameter("name", nameShift).uniqueResult();
+        //rg_resourceEntity.getShiftsById().add(rg_shiftEntity);
+        rg_shiftEntity.getResources().add(rg_resourceEntity);
+
+        //session.beginTransaction();
+        DAOFactory.getResourceInstance().save(session,rg_resourceEntity);
+
+        /*if (isSave) {
+            transaction.commit();
+            System.out.println("保存成功");
+        } else {
+            transaction.rollback();
+            System.out.println("保存失败");
+        }*/
+
     }
 
     public void delete() throws Exception {
@@ -45,10 +84,30 @@ public class ResourceAction extends SuperAction {
     }
 
     public void update() throws Exception {
+        Session session = MySessionFactory.getSessionFactory().openSession();
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+
+        /*JsonNode jsonNode = Tools.jsonTreeModelParse(Tools.getHttpRequestBody(httpServletRequest));
+        String id = jsonNode.get("id").asText();
+        RG_ResourceEntity rg_resourceEntity0 = DAOFactory.getResourceInstance().findAllById(session, id);*/
+
         String jsonString = Tools.getHttpRequestBody(httpServletRequest);
         RG_ResourceEntity rg_resourceEntity = Tools.jsonConvertToEntity(jsonString, RG_ResourceEntity.class);
         ResourceDAOImpl resourceDAOInstance = DAOFactory.getResourceInstance();
-        if (resourceDAOInstance.update(rg_resourceEntity)) {
+
+        String nameShift = rg_resourceEntity.getNameShift();
+
+        RG_ShiftEntity rg_shiftEntity = (RG_ShiftEntity)session.createQuery("select shift from RG_ShiftEntity shift where shift.name =:name").setParameter("name", nameShift).uniqueResult();
+        String shiftId = rg_shiftEntity.getId();
+        /*Set<RG_ShiftEntity> rg_shiftEntity0 = rg_resourceEntity.getShiftsById();
+        rg_shiftEntity.getResources().remove(rg_resourceEntity0);
+        rg_shiftEntity.getResources().clear();
+        rg_shiftEntity.getResources().add(rg_resourceEntity);*/
+
+        session.createNativeQuery("update shift_resource set shift_id = ?").setParameter(1,shiftId).executeUpdate();
+
+        if (resourceDAOInstance.update(session, rg_resourceEntity)) {
         } else {
             WebSocketNotification.broadcast(Tools.creatNotificationMessage("Resource更新失败", "alert"));
         }
